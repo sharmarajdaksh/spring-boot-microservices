f653c3d0c7f4a848f2cd5bd4032305bfc826c03a
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-02-14 14:56:58 +0530

    Added additional call to Ratings service

--- movie-catalog-service/src/main/java/com/sharmarajdaksh/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/sharmarajdaksh/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,7 +2,7 @@ package com.sharmarajdaksh.moviecatalogservice.resources;
 
 import com.sharmarajdaksh.moviecatalogservice.models.CatalogItem;
 import com.sharmarajdaksh.moviecatalogservice.models.Movie;
-import com.sharmarajdaksh.moviecatalogservice.models.Rating;
+import com.sharmarajdaksh.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,13 +22,10 @@ public class MovieCatalogResource {
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // Get all rated movie IDs
-        List<Rating> ratings = List.of(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratingsData/users/" + userId, UserRating.class);
 
         // For each movie ID, call the movie-info-service
-        return ratings.stream().map(rating -> {
+        return ratings.getUserRating().stream().map(rating -> {
                     Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), "Test", rating.getRating());
                 }
--- ratings-data-service/src/main/java/com/sharmarajdaksh/ratingsdataservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/sharmarajdaksh/ratingsdataservice/resources/RatingsResource.java
@@ -1,10 +1,13 @@
 package com.sharmarajdaksh.ratingsdataservice.resources;
 
 import com.sharmarajdaksh.ratingsdataservice.models.Rating;
+import com.sharmarajdaksh.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsData")
@@ -12,6 +15,14 @@ public class RatingsResource {
     @GetMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        return new UserRating(List.of(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        ));
     }
 
 }
